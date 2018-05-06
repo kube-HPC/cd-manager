cf070a5704e4fa6bcc8e9e24c265840780e17a3f
yehiyam <yehiyam@gmail.com>
2018-05-06 09:48:18 +0300

    add install script

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
+cat /etc/hosts
