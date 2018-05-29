626742439f2881b360975c5f9ce8f835148fdb73
yehiyam <yehiyam@gmail.com>
2018-05-29 17:43:38 +0300

    open issue on fail

--- test.sh
+++ test.sh
@@ -1,3 +1,15 @@
+generate_post_data()
+{
+    local failedNoNL="${FAILED//$'\n'/\\n}"
+    cat <<EOF
+{
+    "title":"CD test failed",
+    "body":"# Failed tests  \n${failedNoNL}  \n[Travis-ci logs](https://travis-ci.org/kube-HPC/cd-manager/jobs/${TRAVIS_JOB_ID})"
+}
+EOF
+}
+
+
 echo Running tests!!!
 export API_SERVER_IP=${KUBERNETES_MASTER_IP}
 export ELASTICSEARCH_IP=${KUBERNETES_MASTER_IP}
@@ -6,7 +18,7 @@ export APAK_OUTPUT_PATH=/ApakMCR/example_input_output
 
 # export TEST_FOLDER=$(mktemp -d)/system-test
 export TEST_FOLDER=/tmp/xxx/system-test
-
+export TEST_RESULTS=${TEST_FOLDER}/run-files/results/
 echo cloning system tests to ${TEST_FOLDER}
 mkdir -p ${TEST_FOLDER}
 git clone --depth=1 https://github.com/kube-HPC/system-test.git ${TEST_FOLDER}
@@ -14,7 +26,20 @@ envsubst < ${TEST_FOLDER}/testConfigFiles/template/ipConfigs.template.csv > ${TE
 
 echo using config;
 cat ${TEST_FOLDER}/testConfigFiles/ipConfigs.csv
-docker run --rm -it -v /tmp/xxx/system-test:/system-test  hkube/jmeter:v1.0.1 -c "cd /system-test/run_files; ./regression_indeviduals.sh"
+docker run --rm -it -v /tmp/xxx/system-test:/system-test  hkube/jmeter:v1.0.1 -c "cd /system-test/run_files; ./regression_indeviduals.sh" | tee ./out.log
+
+rc=${PIPESTATUS[0]}
+if [[ $rc != 0 ]]; then
+  echo FAILED
+  awk "/end of run/ { flag = 1 }; flag" out.log > filtered.log
+  export FAILED="$(grep .jtl filtered.log)"
+  echo "${FAILED}" | xargs -n 1 -i sh -c "echo ====== Results for {} ======; cat ${TEST_RESULTS}{}; echo ====== end results ======; echo"
+
+  echo "$(generate_post_data)"
+   curl -X POST -d "$(generate_post_data)" \
+   -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/kube-hpc/cd-manager/issues
+fi
+exit $rc
 # rm -rf ${TEST_FOLDER}
 
 
