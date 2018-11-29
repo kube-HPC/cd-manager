a8ed84ea8b6f42e7cdac113b69487f9c9d407af6
Yehiyam Livneh <yehiyam@gmail.com>
2018-11-29 11:53:00 +0200

    add helm install

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@ echo Deploy to kubernetes
 source ./setPath
 helm repo add hkube http://hkube.io/helm
 helm repo update
-
+envsubst < ./values-pub-template.yml > ~/pub.yml
+helm upgrade -i hkube -f ~/pub.yml hkube/hkube
 # hkube deploy --cluster_name=pub -c
 # until $(curl -k --output /dev/null --silent --head --fail https://${KUBERNETES_MASTER_IP}/hkube/api-server/api/); do
 #     printf '.'
