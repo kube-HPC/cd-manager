39907c0b1680c548f9fd1364a7176b484c9c2e9d
Yehiyam Livneh <yehiyam@gmail.com>
2020-11-30 12:28:44 +0200

    ci - add install

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo Deploy to kubernetes
-source ./setPath
+# source ./setPath
 helm repo add hkube-dev http://hkube.io/helm/dev/
 helm repo update
-envsubst < ./values-pub-template.yml > ~/pub.yml
-helm upgrade --wait --timeout 10m -i hkube -f ~/pub.yml hkube-dev/hkube
+envsubst < ./values-pub-template.yml > /tmp/pub.yml
+helm upgrade --wait --timeout 10m -i hkube -f /tmp/pub.yml hkube-dev/hkube
 helm ls
 
