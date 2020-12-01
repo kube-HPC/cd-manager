085f5b17044a2c81bd85d75388de98bc566c8892
Yehiyam Livneh <yehiyam@gmail.com>
2020-12-01 12:11:17 +0200

    ci - verify chart

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ echo Deploy to kubernetes
 helm repo add hkube-dev http://hkube.io/helm/dev/
 helm repo update
 envsubst < ./values-pub-template.yml > /tmp/pub.yml
+helm search repo hkube
 helm upgrade --wait --timeout 10m -i hkube -f /tmp/pub.yml hkube-dev/hkube
-helm ls
+helm ls --all
 
