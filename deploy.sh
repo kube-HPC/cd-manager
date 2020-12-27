c7d4eb18e6be69d1a6df4826605d473702de5482
yehiyam <yehiyam@users.noreply.github.com>
2020-12-27 14:30:29 +0200

    show helm ls before

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo Deploy to kubernetes
 # source ./setPath
+helm ls --all
 helm repo add hkube-dev http://hkube.io/helm/dev/
 helm repo update
 envsubst < ./values-pub-template.yml > /tmp/pub.yml
