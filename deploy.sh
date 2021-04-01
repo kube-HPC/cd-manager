f713591b84e3443fc7607afc19bc0384c8246c69
Yehiyam Livneh <yehiyam@gmail.com>
2021-04-01 12:18:24 +0300

    wait for version

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,33 @@ echo Deploy to kubernetes
 helm ls --all
 helm repo add hkube-dev http://hkube.io/helm/dev/
 helm repo update
-envsubst < ./values-pub-template.yml > /tmp/pub.yml
-helm search repo hkube
-helm upgrade --wait --timeout 10m -i hkube -f /tmp/pub.yml hkube-dev/hkube
+VERSION=${VERSION:-latest}
+# wait for version
+if [ "$VERSION" != "latest" ]
+then
+    MAX_RETRY=6
+    RETRY=0
+    until [ "$RETRY" -ge "$MAX_RETRY" ]
+    do
+        RETRY=$((RETRY+1))
+        FOUND=$(helm search repo hkube-dev/hkube --version $VERSION)
+        echo $FOUND | grep $VERSION 
+        NOT_FOUND=$?
+        if [ $NOT_FOUND -eq 0 ]
+        then
+            echo found version $VERSION
+            helm upgrade --wait --timeout 10m -i hkube -f /tmp/pub.yml hkube-dev/hkube --version $VERSION
+            break
+        fi
+        echo version $VERSION not ready yet. Retry $RETRY of $MAX_RETRY
+        sleep 10
+    done
+    if [ $RETRY == $MAX_RETRY ]; then
+      echo Failed to install $VERSION. Try running the action again
+    fi
+else
+    helm search repo hkube-dev/hkube
+    helm upgrade --wait --timeout 10m -i hkube -f /tmp/pub.yml hkube-dev/hkube
+fi
 helm ls --all
 
