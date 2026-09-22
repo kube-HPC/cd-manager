81383286ee196ce64d363b2eb6e9e1d9982f6b30
Itamar <itsh13@gmail.com>
2026-09-22 11:26:09 +0300

    feat: add Node.js setup step and update test script for improved environment handling

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@ echo Running tests - short!!!
 export BASE_URL=https://${CLUSTER_URL}
 export TEST_FOLDER=/tmp/xxx/system-test-node
 echo cloning system tests to ${TEST_FOLDER}
+# persistent runner: the previous run's checkout would make git clone fail
+rm -rf ${TEST_FOLDER}
 mkdir -p ${TEST_FOLDER}
 git clone --depth=1 https://github.com/kube-HPC/system-test-node.git ${TEST_FOLDER}
 cd ${TEST_FOLDER}
