2ca7192b8402227f755ff9cac6c84c3542d6e769
Itamar <itsh13@gmail.com>
2026-09-22 11:43:15 +0300

    fix: normalize CLUSTER_URL scheme in test script

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 echo Running tests - short!!!
-export BASE_URL=https://${CLUSTER_URL}
+# CLUSTER_URL may or may not carry a scheme; normalise to exactly one
+case "$CLUSTER_URL" in
+  https://*|http://*) export BASE_URL="$CLUSTER_URL" ;;
+  *) export BASE_URL="https://$CLUSTER_URL" ;;
+esac
+echo "resolved base url host: $(echo "$BASE_URL" | sed -E 's#^https?://##' | cut -d/ -f1)"
 export TEST_FOLDER=/tmp/xxx/system-test-node
 echo cloning system tests to ${TEST_FOLDER}
 # persistent runner: the previous run's checkout would make git clone fail
